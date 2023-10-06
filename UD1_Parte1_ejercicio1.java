90c20f1b282c33ea0fc55e10b4998d3efd4fa794
Erika Toledano <146830919+Erikaatm@users.noreply.github.com>
2023-10-06 10:29:30 +0200

    Mostrar nombre por pantalla

---
+++ UD1_Parte1_ejercicio1.java
@@ -0,0 +1,27 @@
+package unidad1;
+
+// @auothr Erikaatm
+
+public class UD1_Parte1_ejercicio1 {
+ 
+	// Ejercicio de mostrar tu nombre en pantalla; 1.
+	
+	// El main siempre hay que ponerlo para ejecutar los programas.
+		public static void main(String[] args) {
+			
+			
+			// String sirve para texto largo con el que no se vaya a operar.
+			// Defino String nombre como; Erika Toledano.
+			String nombre = "Erika Toledano";
+			
+			// System.out.print--> sirve para imprimir por pantalla en la misma linea.
+			// System.out.println--> sirve para imprimir por pantalla con un enter.
+			
+			// Imprimo por pantalla nombre; Erika Toledano.
+			System.out.println(nombre);
+			
+			// Imprimo por pantalla el nombre; Erika Toledano.
+			System.out.println("Nombre:" + nombre);
+			
+		}
+}
