60966d5d35b35e64eb7de2cc7231fede59873a9b
Erika Toledano <146830919+Erikaatm@users.noreply.github.com>
2023-10-06 10:16:19 +0200

    Imprimir por pantalla un String

---
+++ UD1_Parte1_copia1.java
@@ -0,0 +1,15 @@
+package unidad1;
+
+public class UD1_Parte1_copia1 {
+	public static void main(String[] args) {
+		// Declaracion de variables
+		String textodebienvenida = "Bienvenido a Java";
+		
+		//Sentencias del programa
+		System.out.println(textodebienvenida);
+		
+
+	}
+	//otros métodos
+}
+ 
