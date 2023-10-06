08495470455e9ec4d87b44148aa25aea1e2e56da
Erika Toledano <146830919+Erikaatm@users.noreply.github.com>
2023-10-06 10:24:21 +0200

    Imprimir un String y un Int por pantalla

---
+++ UD1_Parte1_copia4.java
@@ -0,0 +1,11 @@
+package unidad1;
+
+public class UD1_Parte1_copia4 {
+	public static void main(String[] args) {
+		String nombre = "Juan";
+		int edad = 22;
+		System.out.println(nombre);
+		System.out.println(edad);
+		System.out.println(nombre + " tiene " + edad + " años ");
+	}
+}
