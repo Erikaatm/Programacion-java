320aaa4fe7af172591740ff3e989a10492bd3afd
Erika Toledano <146830919+Erikaatm@users.noreply.github.com>
2023-10-06 10:31:21 +0200

    Mostrar en pantalla nombre, dirección y telefono

---
+++ UD1_Parte1_ejercicio2.java
@@ -0,0 +1,31 @@
+package unidad1;
+
+// @author Erikaatm
+
+public class UD1_Parte1_ejercicio2 {
+	
+		// Ejercicio de mostrar tu nombre, dirección y teléfono en pantalla; 2.
+		
+		// El main siempre hay que ponerlo para ejecutar los programas.
+			public static void main(String[] args) {
+				
+				// Defino String nombre como: "Erika Toledano.".
+				String nombre = "Erika Toledano.";
+				
+				// Imprimo por pantalla: " Nombre: Erika Toledano.".
+				System.out.println("Nombre: " + nombre);
+				
+				
+				//Defino el string dirección como: "Calle Avenida de Portugal.". 
+				String direccion = "Calle Avenida de Portugal.";
+				
+				// Defino el string telefono como: "+34 923 12 19 38.". 
+				String telefono = "+34 923 12 19 38.";
+				
+				// Imprimo por pantalla: "Calle Avenida de Portugal.". 
+				System.out.println(direccion);
+				
+				// Imprimo por pantalla: "Teléfono: +34 923 12 19 38.". 
+				System.out.println("Teléfono: " + telefono);		
+	}
+}
