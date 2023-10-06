d96bda7a1cda468208347bbfb3b690ad404a21de
Erika Toledano <146830919+Erikaatm@users.noreply.github.com>
2023-10-06 10:23:09 +0200

    Imprimir Strings en líneas distintas

---
+++ UD1_Parte1_copia3.java
@@ -0,0 +1,9 @@
+package unidad1;
+
+public class UD1_Parte1_copia3 {
+	public static void main(String[] args) {
+		System.out.print("Hola");
+		System.out.println("a todos!!");
+		System.err.println("Esto es un error");
+	}
+}
