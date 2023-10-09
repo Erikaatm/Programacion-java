9e6d7ab4e0081cbfac182e87ab0c7a0596fdb10f
Erika Toledano <146830919+Erikaatm@users.noreply.github.com>
2023-10-09 09:24:10 +0200

    Muestra 10 palabras en Inglés con su traducción

--- UD1_Parte1_ejercicio3.java
+++ UD1_Parte1_ejercicio3.java
@@ -4,7 +4,9 @@ package unidad1;
 
 public class UD1_Parte1_ejercicio3 {
 	public static void main(String[] args) {
+		
 		//Imprimo por pantalla las diez palabras, con un tabulador y su respectiva traducción.
+		// \t; sirve para poner un tabulador.
 		System.out.println("· Bee\t\t · Abeja");
 		System.out.println("· Wasp\t\t · Avispa");
 		System.out.println("· Tree\t\t · Árbol");
