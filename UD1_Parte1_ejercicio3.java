8d70961dc313a83599c0d6dcf1510a93c38c7980
Erika Toledano <146830919+Erikaatm@users.noreply.github.com>
2023-10-09 08:51:32 +0200

    Muestra 10 palabras en Inglés con su traducción

---
+++ UD1_Parte1_ejercicio3.java
@@ -0,0 +1,20 @@
+package unidad1;
+
+//@aouthor Erikaatm
+
+public class UD1_Parte1_ejercicio3 {
+	public static void main(String[] args) {
+		
+		System.out.println("· Abeja\t\t · Bee");
+		System.out.println("· Avispa\t · Wasp");
+		System.out.println("· Árbol\t\t · Tree");
+		System.out.println("· Berenjena\t · Aubergine/Eggplant");
+		System.out.println("· Jirafa\t · Giraffe");
+		System.out.println("· Mariposa\t · Butterfly");
+		System.out.println("· Pelota\t · Ball");
+		System.out.println("· Portátil\t · Laptop");
+		System.out.println("· Salchicha\t · Sausage");
+		System.out.println("· Silla\t\t · Chair");
+		
+	}
+}
