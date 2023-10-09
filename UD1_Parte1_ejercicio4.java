265e6c5fc1ca44429cf49af8ff139960a09ee291
Erika Toledano <146830919+Erikaatm@users.noreply.github.com>
2023-10-09 09:28:49 +0200

    Imprime una pirámide con 9 asteriscos de base

--- UD1_Parte1_ejercicio4.java
+++ UD1_Parte1_ejercicio4.java
@@ -7,11 +7,10 @@ public class UD1_Parte1_ejercicio4 {
 		
 		//Imprimo por pantalla la pirámide utilizando asteriscos, sumando dos en cada fila a partir de la primera.
 		// \n; sirve para hacer un intro.
-		System.out.println("\t\n\n\n\n ");
-		System.out.println("\t    *    ");
-		System.out.println("\t   ***   ");
-		System.out.println("\t  *****  ");
-		System.out.println("\t ******* ");
-		System.out.println("\t*********");
+		System.out.println("\t\n    *    ");
+		System.out.println("\t\n   ***   ");
+		System.out.println("\t\n  *****  ");
+		System.out.println("\t\n ******* ");
+		System.out.println("\t\n*********");
 	}
 }
