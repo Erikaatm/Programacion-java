d7a972861b95349e538f5c80aa4ce85e8d0220d0
Erika Toledano <146830919+Erikaatm@users.noreply.github.com>
2023-10-09 09:35:31 +0200

    Imprimo una pirámide vacía con base 9 asteriscos

---
+++ UD1_Parte1_ejercicio5.java
@@ -0,0 +1,15 @@
+package unidad1;
+
+//@auothor Erikaatm
+
+public class UD1_Parte1_ejercicio5 {
+	public static void main(String[] args) {
+		
+		//Imprimo por pantalla la pirámide utilizando asteriscos con el interior vacío.
+				System.out.println("\n\n\n\n\t    *    ");
+				System.out.println("\t   * *   ");
+				System.out.println("\t  *   *  ");
+				System.out.println("\t *     * ");
+				System.out.println("\t*********");
+	}
+}
